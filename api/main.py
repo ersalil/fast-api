cb9387e8971609aec8add42b6fba05864f569b0e
Yash Janyani <yash.janyani2000@gmail.com>
2022-07-29 13:43:26 +0530

    table data sql implemented

--- api/main.py
+++ api/main.py
@@ -1,10 +1,11 @@
 from wsgiref.simple_server import demo_app
+from anyio import current_time
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import SessionLocal
 from sqlalchemy.sql import func, desc
-import database
-from queries import emb_data
+import database, datetime
+from queries import emb_data, emb_data_bar
 from data import testTableData, testLineData, testBarData, colModel
 
 app = FastAPI()
@@ -54,13 +55,28 @@ def tableView():
     for dictionary1 in es:
         flag = 0
         if len(ls) == 5:
-            return ls
+            break
         for i in ls:
             if i['code'] == dictionary1['code']:
                 flag = 1
         if flag == 0:
             ls.append(dictionary1)
+        for each in ls:
+            # each['starting_date'] = current_time()
+            flag1 = 0
+            flag2 = 0
+            inti = each['checkedin_couch']
+            for each1 in es:
+                if each1['voyage_id'] == each['voyage_id'] and each1['checkedin_couch'] < inti and flag1 == 0:
+                    each['end_date'] = str(datetime.datetime(each1['added_date'].year , each1['added_date'].month , each1['added_date'].day, each1['added_date'].hour, each1['added_date'].minute, each1['added_date'].second)).replace('T', " ")
+                    flag1 = 1
+            # // reverse loop to find the latest date
+            for each2 in es:
+                if each2['voyage_id'] == each['voyage_id'] and each2['checkedin_couch'] == 0 < inti and flag2 == 0:
+                    each['starting_date'] = str(datetime.datetime(each2['added_date'].year , each2['added_date'].month , each2['added_date'].day, each2['added_date'].hour, each2['added_date'].minute, each2['added_date'].second)).replace('T', " ")
+                    flag2 = 1
     return ls
+    
 
 @app.get('/ship/data')
 def shipData():
@@ -80,3 +96,8 @@ def lineData():
 @app.get('/bar/data')
 def barData():
     return testBarData
+
+
+# @app.get('/bar/data/{limit}')
+# def barData(limit):
+#     return emb_data_bar(limit)
