9107c4c16044f63af59df85b8c5e82c14d558a62
Mohammed Zaid <mohommedzaid5@gmail.com>
2022-08-20 01:29:10 +0530

    db

--- db/crud.py
+++ db/crud.py
@@ -37,3 +37,29 @@ def getEmbarkationSummary(db: Session, limit: int):
     except Exception as e:
         raise HTTPException(status_code=500, detail=json.dumps({"message":str(msg), "error": str(e)}))
     return result
+
+# function takes db session and limit and returns the data of the N (N=limit) most recent voyages
+def getEmbarkationManifest(db: Session, limit: int):
+    try:
+        data = db.execute(f"SELECT * FROM get_embark_manifest_sorted({limit})").all()
+        if data is None or data == []:
+            raise HTTPException(status_code=404, detail=msg)
+        result = []
+        for row in data:
+            result.append(dict(row))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=json.dumps({"message":str(msg), "error": str(e)}))
+    return result
+
+# function takes db session and limit and returns the data of the N (N=limit) most recent voyages
+def getEmbarkationBar(db: Session, limit: int):
+    try:
+        data = db.execute(f"SELECT ship, time_int , FLOOR(AVG(diff_checkedin_couch)) as avg_checkedin_count, FLOOR(AVG(diff_onboard_couch)) as avg_onboard_count  FROM get_embark_manifest_sorted({limit}) GROUP BY ship, time_int ORDER BY ship, time_int").all()
+        if data is None or data == []:
+            raise HTTPException(status_code=404, detail=msg)
+        result = []
+        for row in data:
+            result.append(dict(row))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=json.dumps({"message":str(msg), "error": str(e)}))
+    return result
