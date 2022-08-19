58c3a004e855ceb83f8a359bed42d3daab7a96dc
salil agrawal <er.salilagrawal@gmail.com>
2022-08-20 02:25:53 +0530

    updated all the backend, sql, and code functionality with process time headers

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from resources.docs import title, description, tags_metadata, logs_stream, home
 from api import router, lookup
 from config.logging import log, createRequestIdContextvar, getRequestId
 from config.setting import get_settings, Settings
+import time
 
 app = FastAPI(title=title, description=description, openapi_tags=tags_metadata, docs_url="/api/docs")
 
@@ -31,14 +32,15 @@ async def request_middleware(request: Request, call_next):
     log.debug(f"{request.url} : Request started")
 
     try:
+        start_time = time.time()
         response = await call_next(request)
-
+        process_time = time.time() - start_time
     except Exception as ex:
         log.debug(f"{request.url} : Request failed: {ex}")
         response =  JSONResponse(content={"success": False}, status_code=500)
-
     finally:
         response.headers["X-Request-ID"] = getRequestId()
+        response.headers["X-Process-Time"] = str(process_time)
         log.debug(f"{request.url} : Request ended")
         return response 
 
